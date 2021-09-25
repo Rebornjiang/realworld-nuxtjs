f2e75db7a34acc29b580b43b306763a085ad3a52
RebornJiang <626104624@qq.com>
2021-09-25 20:22:49 +0800

    初始化 nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>hello world</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
